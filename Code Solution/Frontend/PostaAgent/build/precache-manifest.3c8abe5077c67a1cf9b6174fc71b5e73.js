ef751ab3014e2a502e6479d704a7db5902785d6a
Arb Koçi <58748458+arbdoescode@users.noreply.github.com>
2022-04-27 09:41:47 +0200

    Add files via upload

---
+++ Code Solution/Frontend/PostaAgent/build/precache-manifest.3c8abe5077c67a1cf9b6174fc71b5e73.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "de12b69f72421626ea53",
+    "url": "/static/css/main.db3ef194.chunk.css"
+  },
+  {
+    "revision": "de12b69f72421626ea53",
+    "url": "/static/js/main.de12b69f.chunk.js"
+  },
+  {
+    "revision": "590e09f014d4134eed07",
+    "url": "/static/css/1.8a62a7d0.chunk.css"
+  },
+  {
+    "revision": "590e09f014d4134eed07",
+    "url": "/static/js/1.590e09f0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d29b392497b077e3fd65e9e291b6f25a",
+    "url": "/static/media/logo2.d29b3924.png"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "89a23824e45477b3774cb41f23dbc18c",
+    "url": "/index.html"
+  }
+];
