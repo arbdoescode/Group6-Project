fbe36afed4af1660a4006ddfa6e9f06ceec6a321
Romeisa Aliu <102466932+RomeisaA@users.noreply.github.com>
2022-06-04 15:04:18 +0200

    Add files via upload

---
+++ Code Solution/Backend/WebApi/AcApi/Models/Response/DailyRaportRes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcApi.Models.Response
+{
+    public class DailyRaportRes //: BaseReq
+    {
+        public List<string> NrPod { get; set; }
+
+        public List<string> KodLevizje { get; set; }
+
+        public List<string> Pershkrimi { get; set; }
+
+        public List<string> MenyraPageses { get; set; }
+
+        public List<string> Destinacioni { get; set; }
+
+        public List<string> KushPaguan { get; set; }
+    }
+}
