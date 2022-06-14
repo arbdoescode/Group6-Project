34de0109224b583ec40e5454905690fb9c641c07
Arb Koçi <58748458+arbdoescode@users.noreply.github.com>
2022-06-14 11:39:40 +0300

    Add files via upload

---
+++ Code Solution/Backend/WebApi/AcApi/Models/Response/UserInfoRes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcApi.Models.Response
+{
+    public class UserInfoRes
+    {
+        public string Username;
+
+        public string Password;
+
+        public long? ClientSubjectID;
+
+        public string Role;
+
+        public bool Aktive;
+    }
+}
