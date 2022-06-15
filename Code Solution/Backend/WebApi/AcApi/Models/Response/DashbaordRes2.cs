1477ec64804020bc9ed2f90934fc7966a2bee400
Arb Koçi <58748458+arbdoescode@users.noreply.github.com>
2022-06-15 16:53:16 +0200

    Add files via upload

---
+++ Code Solution/Backend/WebApi/AcApi/Models/Response/DashbaordRes2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcApi.Models.Response
+{
+    public class DashbaordRes2
+    {
+        public long NrGrumbullime { get; set; }
+
+        public long NrDorezime { get; set; }
+
+        public long NrMosDorezime { get; set; }
+
+        public long NrPorosi { get; set; }
+
+        public long NrPodHub { get; set; }
+
+        public long NrDeleguarKorrier { get; set; }
+
+        public long NrPodAgjensi { get; set; }
+
+        public long NrKthimeKlient { get; set; }
+
+        public long NrRefuzime { get; set; }
+    }
+}
